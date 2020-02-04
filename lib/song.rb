bbe0a98c84484e07f8fd132d4df748fc637d78fe
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-04 18:23:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,54 +1,10 @@
-require 'pry'
 class Song 
- attr_accessor :song, :artist, :genre
-  @@count = 0 
-  @@artist = []
-  @@genre = []
-
-def
-initialize (song,artist,genre)
-  @song= song
-  @artist= artist
-  @genre = genre
-   @@count += 1 
-  @@artist << artist
-  @@genre << genre 
-end 
-
+  attr_accessor :name, :artist, :genre
   
-def self.count
-  @@count
-end
-
-def self.artist
-  @@artist
-end
-
-def self.genre
-  @@genre
-end
-
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre] 
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
+  def initialize(name, artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
   end
-
-def self.artist_count
-  artist_count ={}
-  @@artist.each do |artist|
-    if artist_count[artist]
-      artist_count[artist] += 1 
-    else
-      artist_count[artist] = 1 
-    end
-    artist_count
-end
-end 
+  
 end
